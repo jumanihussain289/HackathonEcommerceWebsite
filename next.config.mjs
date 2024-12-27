357ab001de46f7967a8405a4fa1fc892b5ae8afe
jumanihussain289 <jumanihussain2@gmail.com>
2024-12-28 04:15:50 +0500

    Updated Hackathon Code2

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    webpack(config) {
+      config.module.rules.push({
+        test: /\.svg$/, // Match all .svg files
+        use: ["@svgr/webpack"], // Use the SVGR Webpack loader
+      });
+  
+      return config; // Return the modified config
+    },
+  };
+  
+  export default nextConfig;
+  
--- src/app/components/hero.tsx
+++ src/app/components/hero.tsx
@@ -3,20 +3,27 @@ import React from 'react'
 
 const Hero = () => {
     return (
-        <div className='flex justify-between items-center -translate-y-[-1/2] px-16'>
+        <main className='w-full md:h-[500px] flex flex-col md:flex-row justify-between items-start bg-[#f2f0f1]'>
             {/* Left */}
-            <div className='bg-red-400 w-[400px] space-y-10'>
-                <h1 className='text-5xl font-bold'>FIND CLOTHES  THAT MATCHES YOUR STYLE</h1>
-                <p>Browse through our diverse range of meticulously
+            <div className=' w-full px-3 md:w-[450px] mt-3 md:mt-[70px] md:ml-[100px]'>
+                <h1 className='text-4xl md:text-5xl font-extrabold '>
+                    FIND CLOTHES <br /> THAT MATCHES YOUR STYLE
+                    </h1>
+                <p className='text-xs mt-5 md:mt-3'>Browse through our diverse range of meticulously
                     crafted garments, designed to bring out your individuality
                     and cater to your sense of style.</p>
-                <button>Shop Now</button>
+                <button className='bg-black py-3 px-10 rounded-[25px] text-sm mt-6 w-[330px] sm:w-[150px] md:mt-4 text-white'>Shop Now</button>
             </div>
             {/* Right */}
-            <div>
-                <Image src={""}  alt='profile'></Image>
+            <div className='relative'>
+                {/* Profile Png */}
+                <Image src={"/images/header-homepage.png"} width={200} height={200} alt='profile' className='w-[600px] mt-[17px] sm:w-[504px]'></Image>
+                {/* Small-Star-Left Svg */}
+                <Image src={"/icons/small-star.svg"} width={200} height={200} alt='profile' className=' w-[40px] absolute top-[115px] left-4 md:w-[40px] md:top-[220px] md:-left-4'></Image>
+                {/* Big-Star-Right Svg */}
+                <Image src={"/icons/big-star.svg"} width={200} height={200} alt='profile' className='w-[60px] absolute  top-[30px] left-[270px] md:w-[70px] md:top-[60px] md:left-[400px]'></Image>
             </div>
-        </div>
+        </main>
     )
 }
 
